56fe5e5ac03912e8c53cf76ed075d6a8c73f6d16
dianedouglas <diane.douglas1@gmail.com>
2017-02-26 14:55:39 -0500

    Add initial product model with tests.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '9ec387f6f6baf4ccccb4cc409ce03c513cac05cb9a30b658489fd34e6c7d412d3de6418c16645d6c9cfd0e27b635d073424370dc1f472145f129a50e99289fa7'
