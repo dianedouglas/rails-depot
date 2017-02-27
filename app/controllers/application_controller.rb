0ff3b0255a4e439f70b2519ebd2512902a6f4e2e
dianedouglas <diane.douglas1@gmail.com>
2017-02-27 01:01:45 -0500

    Fix sidebar links and add helper method current_user to show some links if logged out and others if logged in. Also minor cosmetic fixes - product show link wrapped around title, edit user on admin welcome page, nicer forms.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   before_action :authorize
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
   protected
     def authorize
       unless User.find_by(id: session[:user_id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: [:new, :create]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -28,7 +29,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to users_url, notice: 'User was successfully created.' }
+        session[:user_id] = @user.id
+        format.html { redirect_to admin_url }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
